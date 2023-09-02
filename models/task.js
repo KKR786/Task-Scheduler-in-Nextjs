6fc0ccff9624ffd2db63d26d5cea66364c35e802
Khalid Kaiser <romman.kaiser@gmail.com>
2023-09-02 17:15:17 +0600

    Revert "8th - db"
    This reverts commit 9cf62fb4bc41525b45c96c594dcbbe4aa13c65f8.

--- models/task.js
+++
@@ -1,5 +0,0 @@
-import { Schema, model, models } from "mongoose";
-
-const taskSchema = new Schema({
-      
-})
--- models/user.js
+++ models/user.js
@@ -1,10 +1,6 @@
 import { Schema, model, models } from 'mongoose';
-import Sequence from '@/models/sequence';
 
 const userSchema = new Schema({
-    _id: {
-        type: Number
-    },
   email: {
     type: String,
     unique: [true, 'Email already exists!'],
@@ -21,25 +17,7 @@ const userSchema = new Schema({
   image: {
     type: String,
   }
-}, { timestamps: true });
-
-userSchema.pre('save', async function (next) {
-    if (this.isNew) {
-      try {
-        const sequenceDoc = await Sequence.findByIdAndUpdate(
-          'userId',
-          { $inc: { sequence_value: 1 } },
-          { new: true, upsert: true }
-        ).exec();
-  
-        this._id = sequenceDoc.sequence_value;
-      } catch (err) {
-        return next(err);
-      }
-    }
-  
-    next();
-  });
+});
 
 const User = models.User || model("User", userSchema);
 
