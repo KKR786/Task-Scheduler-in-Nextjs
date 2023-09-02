ef23488100f0a55c718e5c82b9b05298084548f1
Khalid Kaiser <romman.kaiser@gmail.com>
2023-09-02 17:14:38 +0600

    8th - db

--- components/form/registration.jsx
+++ components/form/registration.jsx
@@ -7,6 +7,8 @@ function RegistrationComponent() {
     const [designation, setDesignation] = useState('')
     const [password, setPassword] = useState('')
 
+    
+
   return (
     <div>
       <form>
@@ -54,6 +56,7 @@ function RegistrationComponent() {
             style={{
               'background-color': "darkmagenta",
             }}
+            onClick={handleSubmit}
           >
             Sign Up
           </button>
